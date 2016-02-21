0a28aa9fcf24aff76c8ce3953fa5fb94abe97117
tinybearc <tinybearc@gmail.com>
2016-02-21 17:44:10 +0800

    add cpp

--- cpp/Classes/HelloWorldScene.cpp
+++ cpp/Classes/HelloWorldScene.cpp
@@ -2,6 +2,27 @@
 #include "HelloWorldScene.h"
 
 USING_NS_CC;
+using namespace sdkbox;
+
+#define kLevelLeaderBoardId "1434"
+#define kSoldierAchievementId "3622"
+
+class MyLeaderboardListener : public sdkbox::LeaderboardListener
+{
+public:
+    virtual void onComplete(std::string leaderboard)
+    {
+        CCLOG("MyLeaderboardListener:onComplete: %s", leaderboard.data());
+    }
+    virtual void onFail()
+    {
+        CCLOG("MyLeaderboardListener:onFail");
+    }
+};
+
+template <typename T> std::string tostr(const T& t) {
+    std::ostringstream os; os << t; return os.str();
+}
 
 Scene* HelloWorld::createScene()
 {
@@ -56,21 +77,146 @@ bool HelloWorld::init()
 
 void HelloWorld::createTestMenu()
 {
+    IAP::setDebug(true);
+    IAP::setListener(this);
+    IAP::init();
+
+    sdkbox::PluginAchievement::init();
+    sdkbox::PluginLeaderboard::init();
+    sdkbox::PluginLeaderboard::setListener(new MyLeaderboardListener);
+
+    Size size = Director::getInstance()->getWinSize();
+
+    _coinCount = 0;
+    _txtCoin = Label::createWithSystemFont("0", "sans", 24);
+    _txtCoin->setPosition(Vec2(size.width / 2, 60));
+    addChild(_txtCoin);
+
     auto menu = Menu::create();
 
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 1", "sans", 24), [](Ref*){
-        CCLOG("Test Item 1");
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("load products", "sans", 24), CC_CALLBACK_1(HelloWorld::onRequestIAP, this)));
+
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("restore purchase", "sans", 24), CC_CALLBACK_1(HelloWorld::onRestoreIAP, this)));
+
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("update leaderboard random [1,100]", "sans", 24), [](Ref*){
+        int score = rand_0_1() * 100 + 1;
+        PluginLeaderboard::submitScore(kLevelLeaderBoardId, score);
     }));
 
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 2", "sans", 24), [](Ref*){
-        CCLOG("Test Item 2");
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("get leaderboard", "sans", 24), [](Ref*){
+        PluginLeaderboard::getLeaderboard(kLevelLeaderBoardId);
     }));
 
-    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("Test Item 3", "sans", 24), [](Ref*){
-        CCLOG("Test Item 3");
+    menu->addChild(MenuItemLabel::create(Label::createWithSystemFont("achievement test", "sans", 24), [](Ref*){
+        PluginAchievement::unlock(kSoldierAchievementId);
     }));
 
     menu->alignItemsVerticallyWithPadding(10);
     addChild(menu);
 }
 
+// callbacks
+
+void HelloWorld::onShowAds(cocos2d::Ref *sender)
+{
+    CCLOG("Show Ads");
+}
+
+void HelloWorld::onRequestIAP(cocos2d::Ref* sender)
+{
+    IAP::refresh();
+}
+
+void HelloWorld::onRestoreIAP(cocos2d::Ref* sender)
+{
+    IAP::restore();
+}
+
+void HelloWorld::onIAP(cocos2d::Ref *sender)
+{
+    int i = reinterpret_cast<int>(dynamic_cast<MenuItemLabel*>(sender)->getUserData());
+    auto const &product = _products[i];
+    CCLOG("Start IAP %s", product.name.c_str());
+    IAP::purchase(product.name);
+}
+
+void HelloWorld::onInitialized(bool ok)
+{
+    CCLOG("%s : %d", __func__, ok);
+}
+
+void HelloWorld::onSuccess(const Product &p)
+{
+    if (p.name == "coin_package") {
+        CCLOG("Purchase complete: coin_package");
+        _coinCount += 1000;
+        _txtCoin->setString(tostr(_coinCount));
+    }
+    else if (p.name == "coin_package2") {
+        CCLOG("Purchase complete: coin_package2");
+        _coinCount += 5000;
+        _txtCoin->setString(tostr(_coinCount));
+    }
+    else if (p.name == "remove_ads") {
+        CCLOG("Purchase complete: Remove Ads");
+    }
+
+    CCLOG("Purchase Success: %s", p.id.c_str());
+}
+
+void HelloWorld::onFailure(const Product &p, const std::string &msg)
+{
+    CCLOG("Purchase Failed: %s", msg.c_str());
+
+}
+
+void HelloWorld::onCanceled(const Product &p)
+{
+    CCLOG("Purchase Canceled: %s", p.id.c_str());
+}
+
+void HelloWorld::onRestored(const Product& p)
+{
+    CCLOG("Purchase Restored: %s", p.name.c_str());
+}
+
+void HelloWorld::updateIAP(const std::vector<sdkbox::Product>& products)
+{
+    _iapMenu->removeAllChildren();
+    _products = products;
+
+
+    for (int i=0; i < _products.size(); i++)
+    {
+        CCLOG("IAP: ========= IAP Item =========");
+        CCLOG("IAP: Name: %s", _products[i].name.c_str());
+        CCLOG("IAP: ID: %s", _products[i].id.c_str());
+        CCLOG("IAP: Title: %s", _products[i].title.c_str());
+        CCLOG("IAP: Desc: %s", _products[i].description.c_str());
+        CCLOG("IAP: Price: %s", _products[i].price.c_str());
+        CCLOG("IAP: Price Value: %f", _products[i].priceValue);
+
+        auto item = MenuItemLabel::create(Label::createWithSystemFont(_products[i].name, "sans", 24), CC_CALLBACK_1(HelloWorld::onIAP, this));
+        item->setUserData(reinterpret_cast<void*>(i));
+        _iapMenu->addChild(item);
+    }
+
+    Size size = Director::getInstance()->getWinSize();
+    _iapMenu->alignItemsVerticallyWithPadding(5);
+    _iapMenu->setPosition(Vec2(size.width/2, size.height / 2));
+}
+
+void HelloWorld::onProductRequestSuccess(const std::vector<Product> &products)
+{
+    updateIAP(products);
+}
+
+void HelloWorld::onProductRequestFailure(const std::string &msg)
+{
+    CCLOG("Fail to load products");
+}
+
+void HelloWorld::onRestoreComplete(bool ok, const std::string &msg)
+{
+    CCLOG("%s:%d:%s", __func__, ok, msg.data());
+}
--- cpp/Classes/HelloWorldScene.h
+++ cpp/Classes/HelloWorldScene.h
@@ -2,8 +2,11 @@
 #define __HELLOWORLD_SCENE_H__
 
 #include "cocos2d.h"
+#include "PluginIAP/PluginIAP.h"
+#include "PluginAchievement/PluginAchievement.h"
+#include "PluginLeaderboard/PluginLeaderboard.h"
 
-class HelloWorld : public cocos2d::Layer
+class HelloWorld : public cocos2d::Layer, public sdkbox::IAPListener
 {
 public:
     // there's no 'id' in cpp, so we recommend returning the class instance pointer
@@ -18,6 +21,24 @@ public:
 private:
     void createTestMenu();
 
+    void onShowAds(cocos2d::Ref* sender);
+    void onRequestIAP(cocos2d::Ref* sender);
+    void onRestoreIAP(cocos2d::Ref* sender);
+    void onIAP(cocos2d::Ref* sender);
+    void updateIAP(const std::vector<sdkbox::Product>& products);
+    virtual void onInitialized(bool ok) override;
+    virtual void onSuccess(sdkbox::Product const& p) override;
+    virtual void onFailure(sdkbox::Product const& p, const std::string &msg) override;
+    virtual void onCanceled(sdkbox::Product const& p) override;
+    virtual void onRestored(sdkbox::Product const& p) override;
+    virtual void onProductRequestSuccess(std::vector<sdkbox::Product> const &products) override;
+    virtual void onProductRequestFailure(const std::string &msg) override;
+    void onRestoreComplete(bool ok, const std::string &msg);
+
+    cocos2d::Menu * _iapMenu;
+    std::vector<sdkbox::Product> _products;
+    cocos2d::Label* _txtCoin;
+    int _coinCount;
 };
 
 #endif // __HELLOWORLD_SCENE_H__
